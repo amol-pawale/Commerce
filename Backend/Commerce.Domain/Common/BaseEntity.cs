a7b7953bc12b688a619c0c7f0a254385486e530a
amol.pawale <pawale024@gmail.com>
2026-06-16 00:24:12 +0530

    COM added the entity classes

---
+++ Backend/Commerce.Domain/Common/BaseEntity.cs
@@ -0,0 +1,16 @@
+using System; 
+namespace Commerce.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid id {get; protected set} = Guid.NewGuid();
+    public DateTime CreatedAt{get; set;} = DateTime.UtcNow;
+
+    public string CreatedBy{get; set;}
+
+    public DateTime? LastModifiedAt{get; set;}
+
+    public string LastModifiedBy {get; set;}
+    
+
+}
---
+++ Backend/Commerce.Domain/Entities/Product.cs
@@ -0,0 +1,1 @@
+namespace Commerce.Domain.Entities;
