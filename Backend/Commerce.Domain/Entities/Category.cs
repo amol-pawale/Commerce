d43f554f347a14a54711ef16bd058e562a487da5
amol.pawale <pawale024@gmail.com>
2026-06-17 23:56:44 +0530

    COM added categary

--- Backend/Commerce.Domain/Entities/Category.cs
+++ Backend/Commerce.Domain/Entities/Category.cs
@@ -1,2 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Commerce.Domain.Common;
+
 namespace Commerce.Domain.Entities;
-   
+
+public class Category : BaseEntity
+{
+    public string Name {get; private set;} = string.Empty;
+
+    public string? Description {get; private set};
+
+    public readonly List<Category> _subCategories = new();
+    
+}
