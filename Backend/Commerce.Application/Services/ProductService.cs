829ac7a3ffc7bbc105c8569f3797262b27d19a59
amol.pawale <pawale024@gmail.com>
2026-06-30 23:50:31 +0530

    Added product service

---
+++ Backend/Commerce.Application/Services/ProductService.cs
@@ -0,0 +1,35 @@
+using Commerce.Application.DTOs.Responses;
+using Commerce.Application.Interfaces;
+
+namespace Commerce.Application.Services;
+using System.Linq; 
+
+public class ProductService
+{
+    private readonly IProductRepository _repository; 
+
+    public ProductService(IProductRepository repository)
+    {
+        _repository  = repository ;
+    }
+    public async Task<List<ProductResponse>> GetAllAsync()
+    {
+        var products = await _repository.GetAllAsync();
+        List<ProductResponse> productResponses = new();
+        ProductResponse productResponse = new();
+
+        return products.Select(product => new ProductResponse
+        {
+            Id = product.Id,
+            Name = product.Name,
+            SKU = product.SKU,
+            Price = product.Price.Amount,
+            Currency = product.Price.Currency
+            StockQuantity = product
+
+        }).ToList();
+
+
+    }       
+
+}
