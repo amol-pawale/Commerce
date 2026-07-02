bce659a374c9780c58b5a712d396b41d7520a7f7
amol.pawale <pawale024@gmail.com>
2026-07-02 16:44:04 +0530

    format change

--- Backend/Commerce.Application/DTOs/Common/PagedResult.cs
+++ Backend/Commerce.Application/DTOs/Common/PagedResult.cs
@@ -3,7 +3,7 @@ namespace Commerce.Application.DTOs.Common;
 
 public class PagedResult<T>
 {
-    public IReadOnlyList<T> Items { get; init; }  = Array.Empty<T>();
+    public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
     public int PageNumber { get; init; }
     public int PageSize { get; init; }
     public int TotalCount { get; init; }
