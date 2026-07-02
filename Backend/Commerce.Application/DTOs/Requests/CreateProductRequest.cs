4e764f86833fe07261338bcb8f2ae15ce530da19
amol.pawale <pawale024@gmail.com>
2026-07-02 16:22:13 +0530

    updated the validations

--- Backend/Commerce.Application/DTOs/Requests/CreateProductRequest.cs
+++ Backend/Commerce.Application/DTOs/Requests/CreateProductRequest.cs
@@ -9,12 +9,12 @@ public class CreateProductRequest
     [Required, StringLength(200)]
     public string Name { get; set; }
 
-    [Range(0.01, double.MaxValue)]
+    [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
     public decimal Price { get; set; }
 
     [Required, StringLength(3, MinimumLength = 3)]
     public string Currency { get; set; }
 
-    [Range(1, int.MaxValue)]
+    [Range(0, int.MaxValue)]
     public int StockQuantity { get; set; }
 }
